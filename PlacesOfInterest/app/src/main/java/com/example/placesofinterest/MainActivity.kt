0ea838db94c5d3e3adf2fc07420328e73ee3506e
Unknown <hugodekruijf@hotmail.com>
2020-04-07 19:56:24 +0200

    level 2 task1 final step

--- PlacesOfInterest/app/src/main/java/com/example/placesofinterest/MainActivity.kt
+++ PlacesOfInterest/app/src/main/java/com/example/placesofinterest/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.placesofinterest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var places = arrayListOf<Place>()
+    private var placeAdapter = PlaceAdapter(places)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private  fun initViews(){
+        recvPlaces.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        recvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanged()
     }
 }
