ea1f6b1279b76d5161bc4dad2cdeca414a74312f
Unknown <hugodekruijf@hotmail.com>
2020-04-10 10:22:53 +0200

    level 3 task 1 final step

--- UserProfile/app/src/main/java/com/example/userprofile/ProfileActivity.kt
+++ UserProfile/app/src/main/java/com/example/userprofile/ProfileActivity.kt
@@ -2,11 +2,13 @@ package com.example.userprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_profile.*
 
 const val PROFILE_EXTRA = "PROFILE_EXTRA"
 
 class ProfileActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +17,8 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "This is your profile!"
         val profile = intent.getParcelableExtra<Profile>(PROFILE_EXTRA)
 
         if (profile != null) {
@@ -23,5 +27,16 @@ class ProfileActivity : AppCompatActivity() {
             if (profile.imageUri != null) ivProfileImage.setImageURI(profile.imageUri)
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 
 }
