7f54d5c45f73cce6e34c220c4e856f97b8316dfd
Unknown <hugodekruijf@hotmail.com>
2020-04-13 15:59:50 +0200

    level 4 task 1 step 2

---
+++ level4task1/app/src/main/java/com/example/level4task1/ProductDao.kt
@@ -0,0 +1,22 @@
+package com.example.level4task1
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.androidcourse.shoppinglistkotlin.model.Product
+
+@Dao
+interface ProductDao {
+    @Query("SELECT * FROM product_table")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query("DELETE FROM product_table")
+    suspend fun deleteAllProducts()
+}
