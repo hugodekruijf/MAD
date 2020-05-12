df8d55b4687253c932cdddfaf6e1347ccfefe4d5
Unknown <hugodekruijf@hotmail.com>
2020-05-12 21:15:34 +0200

    level 5 task 1 step 3 and 4

--- level5task1/app/src/main/java/com/example/level5task1/ui/main/MainActivity.kt
+++ level5task1/app/src/main/java/com/example/level5task1/ui/main/MainActivity.kt
@@ -5,22 +5,40 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.level5task1.R
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var  mainActivityViewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        initViewModel()
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
+
+    private fun initViewModel() {
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        mainActivityViewModel.note.observe(this, Observer { note ->
+            if (note != null) {
+                tvTitle.text = note.title
+                tvLastUpdated.text = getString(R.string.last_updated, note.lastUpdated.toString())
+                tvNote.text = note.text
+            }
+        })
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
