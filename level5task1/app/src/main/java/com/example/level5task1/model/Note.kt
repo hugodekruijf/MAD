b4e4a186c1ca8d51fe12c7dabd78f6f9b9fae5c0
Unknown <hugodekruijf@hotmail.com>
2020-05-12 20:56:02 +0200

    level 5 task 1 step 2

---
+++ level5task1/app/src/main/java/com/example/level5task1/model/Note.kt
@@ -0,0 +1,15 @@
+package com.example.level5task1.model
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+@Entity
+data class Note(
+    var title: String,
+    var lastUpdated: Date,
+    var text: String,
+    @PrimaryKey var id : Long? = null
+) : Parcelable
