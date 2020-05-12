0a5dd00202c87bebc547b2a3408e33ff983eafdd
Unknown <hugodekruijf@hotmail.com>
2020-05-12 20:48:36 +0200

    level 5 task 1 step 1

--- level5task1/app/src/main/java/com/example/level5task1/ui/main/MainActivity.kt
+++ level5task1/app/src/main/java/com/example/level5task1/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.level5task1
+package com.example.level5task1.ui.main
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.level5task1.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
