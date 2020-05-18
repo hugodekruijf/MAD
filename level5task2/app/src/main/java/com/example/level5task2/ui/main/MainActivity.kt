70bed8e53dc7979b57e1fc803c048367a0ba9b88
Unknown <hugodekruijf@hotmail.com>
2020-05-18 20:22:45 +0200

    level 5 task 2 new basic activity for adding game

--- level5task2/app/src/main/java/com/example/level5task2/ui/main/MainActivity.kt
+++ level5task2/app/src/main/java/com/example/level5task2/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.level5task2
+package com.example.level5task2.ui.main
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.level5task2.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
