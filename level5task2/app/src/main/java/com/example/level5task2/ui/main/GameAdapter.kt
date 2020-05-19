80748bb450b5ec2ae5d1a8f8da3a2ff335025c1f
Unknown <hugodekruijf@hotmail.com>
2020-05-19 18:12:11 +0200

    level 5 task 2 adapter + itemxml

---
+++ level5task2/app/src/main/java/com/example/level5task2/ui/main/GameAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.level5task2.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.level5task2.R
+import com.example.level5task2.model.Game
+import kotlinx.android.synthetic.main.item_game.view.*
+
+class GameAdapter(private val games: List<Game>) : RecyclerView.Adapter<GameAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(game: Game) {
+            itemView.textTitle.text = game.title
+            itemView.textPlatform.text = game.platfrom
+            itemView.textDate.text = game.day + game.month + game.year
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_game, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return games.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(games[position])
+    }
+}
