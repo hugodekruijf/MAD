cafcd838df154a00ba00102e882374f12f6c3d04
Unknown <hugodekruijf@hotmail.com>
2020-05-22 10:53:36 +0200

    TaskPlanner packages main layout

--- TaskPlanner/app/src/main/java/com/example/taskplanner/ui/main/MainActivity.kt
+++ TaskPlanner/app/src/main/java/com/example/taskplanner/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.taskplanner
+package com.example.taskplanner.ui.main
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.taskplanner.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
