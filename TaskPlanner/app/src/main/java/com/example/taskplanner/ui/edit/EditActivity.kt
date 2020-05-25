e49d0f771be9538a866b4e2e1d8211609ad6bf95
Unknown <hugodekruijf@hotmail.com>
2020-05-25 20:55:13 +0200

    TaskPlanner editActivity edit Layout

---
+++ TaskPlanner/app/src/main/java/com/example/taskplanner/ui/edit/EditActivity.kt
@@ -0,0 +1,13 @@
+package com.example.taskplanner.ui.edit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taskplanner.R
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
